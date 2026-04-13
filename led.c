181ec80f1e257911dbf2aed9afdd1f7140961832
srinivasulusballi <srinivasulus614@gmail.com>
2026-04-13 16:36:56 +0530

    Quality (#3)
    * Create led.c (#1)
    
    led
    
    * Update led.c (#2)

---
+++ led.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>  // for sleep()
+
+int main() {
+    while (1) {
+        printf("LED ON\n");
+        sleep(10);   // wait 1 second
+
+        printf("LED OFF\n");
+        sleep(1);   // wait 1 second
+    }
+    return 0;
+}
